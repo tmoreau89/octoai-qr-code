3d836b2da4bf25800f699fe49dc666a12ee1d319
Thierry Moreau <tmoreau@octoml.ai>
2023-07-21 17:46:08 -0400

    one version at a time to save compute

--- octoqr.py
+++ octoqr.py
@@ -15,7 +15,7 @@ def transform_qr_code(url, food_type):
     qr = qrcode.QRCode(
         version=3,
         box_size=10,
-        border=1,
+        border=3,
         error_correction=qrcode.ERROR_CORRECT_H)
     qr.add_data(url)
     qr.make(fit=True)
@@ -28,33 +28,27 @@ def transform_qr_code(url, food_type):
     img = qr_code.convert('RGB')
 
     seed = random.randint(0,1000)
-    for i in range(0, 3):
-        unit1 = webuiapi.ControlNetUnit(
-            input_image=img,
-            module="invert",
-            weight=1.25,
-            guidance_start=0,
-            guidance_end=0.90,
-            model="control_v1p_sd15_qrcode_monster [5e5778cb]"
-        )
-        res = api.txt2img(
-            prompt=food_type+", RAW photo, <lora:foodphoto:0.8> foodphoto, soft lighting, high quality, film grain, Fujifilm XT",
-            seed=seed+i,
-            cfg_scale=7,
-            steps=30,
-            width=768,
-            height=768,
-            n_iter=1,
-            sampler_name="Euler a",
-            controlnet_units=[unit1],
-            override_settings={"sd_model_checkpoint": "realistic.safetensors"},
-        )
-        if i % 3 == 0:
-            col1.image(res.images[0])
-        elif i % 3 == 1:
-            col2.image(res.images[0])
-        elif i % 3 == 2:
-            col3.image(res.images[0])
+    unit1 = webuiapi.ControlNetUnit(
+        input_image=img,
+        module="invert",
+        weight=1.25,
+        guidance_start=0,
+        guidance_end=0.90,
+        model="control_v1p_sd15_qrcode_monster [5e5778cb]"
+    )
+    res = api.txt2img(
+        prompt=food_type+", RAW photo, <lora:foodphoto:0.8> foodphoto, soft lighting, high quality, film grain, Fujifilm XT",
+        seed=seed,
+        cfg_scale=7,
+        steps=20,
+        width=768,
+        height=768,
+        n_iter=1,
+        sampler_name="Euler a",
+        controlnet_units=[unit1],
+        override_settings={"sd_model_checkpoint": "realistic.safetensors"},
+    )
+    st.image(res.images[0])
 
 st.set_page_config(layout="wide", page_title="QR Chef")
 
@@ -63,8 +57,6 @@ st.write("## QR Chef - Powered by OctoAI")
 url = st.text_input("URL to your webpage!", "https://octoml.ai/")
 
 food_type = st.text_input("Your favorite food!", "nachos")
-
-col1, col2, col3 = st.columns(3)
 
 if st.button('(re-)Generate QR code!'):
     transform_qr_code(url, food_type)
